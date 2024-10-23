bcbc461ec2525619f0d88655205f418e03127d49
vinyas-bharadwaj <vinyasbharadwaj101@gmail.com>
2024-10-24 00:41:07 +0530

    added icon and banner fields, added validators for those fields

--- src/src/urls.py
+++ src/src/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 from server.views import ServerListViewSet
@@ -29,3 +31,6 @@ urlpatterns = [
     path('api/docs/schema/', SpectacularAPIView.as_view(), name="schema"),
     path('api/docs/schema/ui', SpectacularSwaggerView.as_view()),
 ] + router.urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
